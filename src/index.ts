790441d112bd6e33201f651afd70c98c9e336ac6
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-04-29 17:31:31 +0900

    added route param for courses

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import { Application, Request, Response } from "express";
+
 const express = require("express");
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("kldsafjdklf");
+app.get("/courses/:id", (req: Request, res: Response) => {
+  res.send(req.params);
 });
 
-app.get('/api/courses', (req: Request, res: Response) => {
-  res.send([1, 2, 3]);
-})
+const port = process.env.PORT || 3001;
 
-app.listen(3001, () => {
-  console.log("Listening on port 3001...");
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
 });
