b89ddf759fe3a23a70db0aa8a41d77f032a0f770
IslomJonMirzakbarov <islomjonmirzakbarov@gmail.com>
2022-04-29 14:50:37 +0900

    added two get request routes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import express, { Request, Response, Application } from "express";
+import { Application, Request, Response } from "express";
+const express = require("express");
 
 const app: Application = express();
 
-const port = 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('kldsafdskl');
+app.get("/", (req: Request, res: Response) => {
+  res.send("kldsafjdklf");
 });
 
-app.listen(port, () => {
-  console.log(`app is runnung on https://localhost:${port}`);
+app.get('/api/courses', (req: Request, res: Response) => {
+  res.send([1, 2, 3]);
 })
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001...");
+});
